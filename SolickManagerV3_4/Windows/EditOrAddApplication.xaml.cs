55877e08e6953da6b952ead0170606f5e06ba3a7
Владимир Булыгин <vovan200512345@mail.ru>
2023-04-06 23:23:01 +1000

    Создал окно редактирования и добавления заявок

---
+++ SolickManagerV3_4/Windows/EditOrAddApplication.xaml.cs
@@ -0,0 +1,39 @@
+﻿using SolickManagerV3_4.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SolickManagerV3_4.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для EditOrAddApplication.xaml
+    /// </summary>
+    public partial class EditOrAddApplication : Window
+    {
+        public string Text { get; set; }
+
+
+        // Данные 
+        public string Data { get; set; }
+        public Client Client { get; set; }
+        public Clientsdevice Device { get; set; }
+        public string Problem { get; set; }
+        public string Notes { get; set; }
+
+
+        public EditOrAddApplication()
+        {
+            InitializeComponent();
+        }
+    }
+}
